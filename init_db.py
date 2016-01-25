964554d15a61b4bf6ddef9e5e790c6c0d413a0c8
yaggul <krommul@gmail.com>
2016-01-25 20:15:59 +0200

    adding program files

---
+++ init_db.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+
+class DBinit:
+    def __init__(self):
+        pass
+
+    def connect_db(self):
+        pass
+
+    def create_tables(self):
+        pass
+
+    def import_data_to_tables(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
